3d624b37c7301b4b750875024011458ad04d9822
datdenkikniet <jcdra1@gmail.com>
2026-04-07 20:03:05 +0200

    Don't use fenix

--- flake.nix
+++ flake.nix
@@ -1,61 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     inputs@{ self, nixpkgs, ... }:
     let
-      # forAllSystems follows this guide:
-      # https://github.com/Misterio77/nix-starter-configs/issues/64#issuecomment-1941420712
-      pkgsFor =
-        system:
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.fenix.overlays.default
-            ];
-          };
-          rustToolchain = pkgs.fenix.fromToolchainFile {
-            dir = ./.;
-            sha256 = "sha256-zC8E38iDVJ1oPIzCqTk/Ujo9+9kx9dXq7wAwPMpkpg0=";
-          };
-          rustPlatform = pkgs.makeRustPlatform {
-            cargo = rustToolchain;
-            rustc = rustToolchain;
-          };
-        in
-        {
-          inherit
-            system
-            pkgs
-            rustToolchain
-            rustPlatform
-            ;
-        };
-
       systems = [
         "aarch64-linux"
         "x86_64-linux"
       ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (pkgsFor system));
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
     in
     {
       devShells = forAllSystems (
-        { pkgs, rustToolchain, ... }:
+        { pkgs, ... }:
         {
           default =
             with pkgs;
             mkShell {
               packages = [
                 nix
-                rustToolchain
+                rustc
+                cargo
                 mdbook
               ];
             };
@@ -63,9 +32,9 @@
       );
       formatter = forAllSystems ({ pkgs, ... }: pkgs.nixfmt-tree);
       packages = forAllSystems (
-        { pkgs, rustPlatform, ... }:
+        { pkgs, ... }:
         {
-          default = rustPlatform.buildRustPackage {
+          default = pkgs.rustPlatform.buildRustPackage {
             pname = "mdbook-typst";
             version = "0.1.0";
             src = ./mdbook-typst;
